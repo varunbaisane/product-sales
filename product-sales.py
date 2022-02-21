5c95c5f9c5ec47dec139dbfa0d7a7b16e378a375
Varun Baisane <64733545+varunbaisane@users.noreply.github.com>
2022-02-21 23:01:36 +0530

    Final code

--- product-sales.py
+++ product-sales.py
@@ -1,5 +1,7 @@
-print('Product Sales Management')
-print('Enter Your Product Details for Two Halves of the Year -->')
+print("+--------------------------+")
+print('| Product Sales Management |')
+print("+--------------------------+")
+print("\nWelcome to Our Project :)")
 h1_table = []
 h2_table = []
 col_heading = ['PRODUCT ID', 'NAME', 'COST PRICE', 'SELLING PRICE', 'UNITS SOLD', 'TOTAL AMOUNT', 'PROFIT', 'PROFIT %']
@@ -9,20 +11,21 @@ h2_table.append(col_heading)
 def enter_data():
     data_choice = 'yes'
     while data_choice.lower() == 'yes':
-        prod_id = input('\ni)Enter the ID of the Product: ')
-        prod_name = input('\nii)Enter the Name of your Product: ')
-        prod_cp = float(input('\niii)Enter the Cost Price of the Product: '))
+        print('\nEnter the following details about your product :- ')
+        prod_id = input('\t\ti)Enter the ID of the Product:=> ')
+        prod_name = input('\t\tii)Enter the Name of your Product:=> ')
+        prod_cp = float(input('\t\tiii)Enter the Cost Price of the Product:=> '))
         prod_cp = round(prod_cp, 2)
 
-        print('\nI] Now Enter the values for First Half of the Year -->')
-        h1_prod_sp = float(input('\ni)Enter the Selling Price of the Product: '))
+        print('\nI] Now Enter the values for First Half of the Year: ')
+        h1_prod_sp = float(input('\t\ti)Enter the Selling Price of the Product:=> '))
         h1_prod_sp = round(h1_prod_sp, 2)
-        h1_prod_sold = int(input('\nii)Enter the number of Units Sold: '))
+        h1_prod_sold = int(input('\t\tii)Enter the number of Units Sold:=> '))
 
-        print('\nII] Now Enter the values for First Half of the Year -->')
-        h2_prod_sp = float(input('\ni)Enter the Selling Price of the Product: '))
+        print('\nII] Now Enter the values for Second Half of the Year:')
+        h2_prod_sp = float(input('\t\ti)Enter the Selling Price of the Product:=> '))
         h2_prod_sp = round(h2_prod_sp, 2)
-        h2_prod_sold = int(input('\nii)Enter the number of Units Sold: '))
+        h2_prod_sold = int(input('\t\tii)Enter the number of Units Sold:=> '))
 
         h1_cp_tot = h1_prod_sold * prod_cp
         h1_sp_tot = h1_prod_sold * h1_prod_sp
@@ -37,24 +40,43 @@ def enter_data():
         h1_table.append([prod_id, prod_name, prod_cp, h1_prod_sp, h1_prod_sold, h1_sp_tot, h1_profit, h1_profit_pc])
         h2_table.append([prod_id, prod_name, prod_cp, h2_prod_sp, h2_prod_sold, h2_sp_tot, h2_profit, h2_profit_pc])
 
-        data_choice = input('\nWant to Enter data for next Product?\nEnter "Yes" to enter data and Enter "No" to Exit: ')
+        data_choice = input('\n\nWant to Enter data for next Product?\n\t\t"Yes" - Enter Data\n\t\t"Any key/No" - Exit:\n\t\t:=> ')
 
+def limit_prod():
+    for i in range(len(h1_table)):
+        if len(h1_table[i][1]) > 10:
+            h1_table[i][1] = h1_table[i][1][:8] + '...'
+    for i in range(len(h2_table)):
+        if len(h2_table[i][1]) > 10:
+            h2_table[i][1]=h2_table[i][1][:8] + '...'
+
+def display(table_list):
+    table_len = -1
+    for row in range(len(table_list)):
+        for ind in range(len(table_list[row])):
+            if len(str(table_list[row][ind])) > table_len:
+                table_len = len(str(table_list[row][ind]))
+
+    limit_prod()
+    for row in range(len(table_list)):
+        for col in range(len(table_list[row])):
+            print('| {:<{}} '.format(table_list[row][col], table_len), end=' |')
+        print()
+                
 def display_h1():
     h1_len = -1
     for row in range(len(h1_table)):
         for ind in range(8):
             if len(str(h1_table[row][ind])) > h1_len:
                 h1_len = len(str(h1_table[row][ind]))
-                h1_w = h1_table[row][ind]
 
-    h1_width = len(str(h1_w))
-
-    print('  --------------------  ')
+    print('\n\n+----------------------+')
     print('| 1ST HALF OF THE YEAR |')
-    print('  --------------------  ')
+    print('+----------------------+\n')
+    limit_prod()
     for row in range(len(h1_table)):
         for col in range(8):
-            print('| {:<{}} '.format(h1_table[row][col], h1_width), end=' |')
+            print('| {:<{}} '.format(h1_table[row][col], h1_len), end=' |')
         print()
 
 def display_h2():
@@ -63,26 +85,112 @@ def display_h2():
         for ind in range(8):
             if len(str(h2_table[row][ind])) > h2_len:
                 h2_len = len(str(h2_table[row][ind]))
-                h2_w = h2_table[row][ind]
-
-    h2_width = len(str(h2_w))
-
-    print('  --------------------  ')
-    print('\n \n| 2ND HALF OF THE YEAR |')
-    print('  --------------------  ')
+    print('\n\n+----------------------+')
+    print('| 2ND HALF OF THE YEAR |')
+    print('+----------------------+\n')
+    limit_prod()
     for row in range(len(h2_table)):
         for col in range(8):
-            print('| {:<{}} '.format(h2_table[row][col], h2_width), end=' |')
+            print('| {:<{}} '.format(h2_table[row][col], h2_len), end=' |')
         print()
 
 def display_prod():
-    print()#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    print('\nProduct IDs:')
+    for k in range(len(h1_table)-1):
+        print(h1_table[k+1][0])
+    prod_choice = input('\nEnter the ID of the Product for Display:=> ')
+    head_col = ['SELLING PRICE', 'UNITS SOLD', 'TOTAL AMOUNT', 'PROFIT', 'PROFIT %']
+    details_head = ['Product ID', 'Product Name', 'Product Cost Price']
+    print() 
+    prod_details = []
+    prod_list = []
+    prod_list.append([])
+    prod_list[0].extend([' ', '1st Half', '2nd Half'])
+    for i in range(len(h1_table)):
+        if prod_choice == h1_table[i][0]:
+            for y in range(3):
+                prod_details.append([])
+                prod_details[y].append(details_head[y])
+                prod_details[y].append(h1_table[i][y])
+            for x in range(5):
+                prod_list.append([])
+                prod_list[x+1].append(head_col[x])
+                prod_list[x+1].append(h1_table[i][x+3])
+                prod_list[x+1].append(h2_table[i][x+3])
+            break
+    print()
+    print('+-----------------+')
+    print('| PRODUCT DETAILS |')
+    print('+-----------------+')
+    print()
+    display(prod_details)
+    print()
+    display(prod_list)
+    print()
 
 def update_data():
-    print()#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    print('\nProduct IDs:')
+    for k in range(len(h1_table)-1):
+        print(h1_table[k+1][0])
+    update_id = input("\nEnter the ID of the  Product which you want to Update\n:=> ")
+    ud_choice=int(input("Enter command for Updatation: \n\t1 - Product ID \n\t2 - Product Name \n\t3 - Product Cost Price \n\t4 - Product Selling Price \n\t5 - Products Sold \n\t:=> "))
+    for k in range(len(h1_table)):
+        if h1_table[k][0] == update_id:
+            if ud_choice == 1:
+                h1_table[k][0] = input('Enter New Product ID:=> ')
+                h2_table[k][0] = h1_table[k][0]
+                break
+            elif ud_choice == 2:
+                h1_table[k][1] = input('Enter New Product Name:=> ')
+                h2_table[k][1] = h1_table[k][1]
+                break
+            elif ud_choice == 3:
+                h1_table[k][2] = input('Enter New Product Cost Price:=> ')
+                h2_table[k][2] = h1_table[k][2]
+                h1_table[k][6] = (h1_table[k][3] * h1_table[k][4]) - (h1_table[k][4] * h1_table[k][2])
+                h1_table[k][7] = round((h1_table[k][6])/(h1_table[k][4] * h1_table[k][2]) * 100,2)
+                h2_table[k][6] = (h2_table[k][3] * h2_table[k][4]) - (h2_table[k][4] * h2_table[k][2])
+                h2_table[k][7] = round((h2_table[k][6])/(h2_table[k][4] * h2_table[k][2]) * 100,2)
+                break
+            elif ud_choice==4:
+                ud_yr_choice = int(input('Enter:\n\t1 - First Half\n\t2 - Second Half'))
+                if ud_yr_choice == 1:
+                    h1_table[k][3]=input('Enter New Product Selling Price:=> ')
+                    h1_table[k][5] = h1_table[k][4] * h1_table[k][3]
+                    h1_table[k][6] = h1_table[k][5] - (h1_table[k][4] * h1_table[k][2])
+                    h1_table[k][7] = round(h1_table[k][6] - (h1_table[k][4] * h1_table[k][2]), 2)
+                    break
+                elif ud_yr_choice == 2:
+                    h2_table[k][3]=input('Enter New Product Selling Price:=> ')
+                    h2_table[k][5] = h2_table[k][4] * h2_table[k][3]
+                    h2_table[k][6] = h2_table[k][5] - (h2_table[k][4] * h2_table[k][2])
+                    h2_table[k][7] = round(h2_table[k][6] - (h2_table[k][4] * h2_table[k][2]), 2)
+                    break
+            elif ud_choice==5:
+                ud_yr_choice = int(input('Enter:\n\t1 - First Half\n\t2 - Second Half'))
+                if ud_yr_choice == 1:
+                    h1_table[k][4] = input('Enter New Number of Products Sold:=> ')
+                    h1_table[k][5] = h1_table[k][4] * h1_table[k][3]
+                    h1_table[k][6] = h1_table[k][5] - (h1_table[k][4] * h1_table[k][2])
+                    h1_table[k][7] = round(h1_table[k][6] - (h1_table[k][4] * h1_table[k][2]), 2)
+                    break
+                elif ud_yr_choice == 2:
+                    h2_table[k][4]=input('Enter New Number of Products Sold:=> ')
+                    h2_table[k][5] = h2_table[k][4] * h2_table[k][3]
+                    h2_table[k][6] = h2_table[k][5] - (h2_table[k][4] * h2_table[k][2])
+                    h2_table[k][7] = round(h2_table[k][6] - (h2_table[k][4] * h2_table[k][2]), 2)
+                    break
+    print('\nAfter Updatation:\n')
+    display_h1()
+    display_h2()
+    print()
 
 def delete_prod():
+    print('\nProduct IDs:')
+    for k in range(len(h1_table)-1):
+        print(h1_table[k+1][0])
     del_id = input("Enter the Product ID which you want to Delete: ")
+    print('\nAfter Deletion: ')
     for i in range(0, len(h1_table)):
         if h1_table[i][0] == del_id:
             h1_table.pop(i)
@@ -95,21 +203,23 @@ def delete_prod():
     display_h2()
 
 def sort_data():
-    print()#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    h1_copy = h1_table.copy()
+    for x in range(1,len(h1_copy)-1):
+        if float(h1_copy[x][2]) > float(h1_copy[x+1][2]):
+            h1_copy[x],h1_copy[x+1] = h1_copy[x+1],h1_copy[x]
+    display(h1_copy)  
 
 choice = 5
-#main loop
 while choice == 5:
     enter_data()
 
     choice = 1
     while choice != 5:
-        choice = int(input('Commands to Perform Action:\n1 - Display Data \n2 - Update Data\n3 - Delete Particular Product Data\n4 - Sort Data\n5 - Enter more Product Data\n6 - Enter "no" to Exit\n:=>'))
+        choice = int(input('\nCommands to Perform Action:\n\t\t1 - Display Data \n\t\t2 - Update Data\n\t\t3 - Delete Particular Product Data\n\t\t4 - Sort Data according to Cost Price\n\t\t5 - Enter more Product Data\n\t\t6 - Exit\n\t\t:=> '))
 
-        #*Display Data
         if choice == 1:
 
-            dis_choice = input('Commands to Display Data:\n Enter "h1" to Display Data for First Half of the Year\n Enter "h2" to Display Data for Second Half of the Year\n Enter "bh" to Display Data for Both Halves of the Year\n Enter "Pd" to Display Data for Particular Product(NOT DONE YET)\n:=>')
+            dis_choice = input('\n\nCommands to Display Data:\n\t\tEnter "h1" - Display Data for First Half of the Year\n\t\tEnter "h2" -  Display Data for Second Half of the Year\n\t\tEnter "bh" - Display Data for Both Halves of the Year\n\t\tEnter "Pd" - Display Data for Particular Product\n\t\t:=> ')
 
             if dis_choice.lower() == 'h1':
                 display_h1()
@@ -123,65 +233,25 @@ while choice == 5:
 
             elif dis_choice.lower() == 'pd':
                 display_prod()
-                print('Work under process...')
 
             else:
-                print('Oops some error')
+                print('Enter valid command.')
         
-        #*Update Data
         elif choice == 2:
-            print('Not yet')
+            update_data()
         
-        #*Delete Data
         elif choice == 3:
             delete_prod()
+
+        elif choice == 4:
+            sort_data()
         
-        #*Again Enter Data
         elif choice == 5:
             print()
         
         elif choice == 6:
+            print('Thank You :)')
             break
 
         else:
-            print('Error...')
-
-#~tfkfvuygkyugjb~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-update_id = input("Enter the id no which u want to update")
-n = int(input("Enter following nos. to update data from \n1-product id \n2-product name \n3-product cost price \n4-first half product selling price \n5-first half product sold"))
-m = input("Enter ur new data")
-for k in range(0, len(h1_table)):
-    if h1_table[k][0] == update_id:
-        if n == 1:
-            h1_table[k][0] = m
-        elif n == 2:
-            h1_table[k][1] = m
-        elif n == 3:
-            h1_table[k][2] = m
-        elif n == 4:
-            h1_table[k][3] = m
-        elif n == 5:
-            h1_table[k][4] = m
-        elif n > 5:
-            print("Error...")
-display_h1()
-
-update_id2 = input("Enter the id no which u want to update")
-q = int(input("Enter following nos. to update data from \n1-product id \n2-product name \n3-product cost price \n4-first half product selling price \n5-first half product sold"))
-p = input("Enter ur new data")
-for u in range(0, len(h2_table)):
-    if h2_table[u][0] == update_id2:
-        if q == 1:
-            prod_id = p
-        elif q == 2:
-            prod_name = p
-        elif q == 3:
-            prod_cp = p
-        elif q == 4:
-            h2_prod_sp = p
-        elif q == 5:
-            h2_prod_sold = p
-        elif q > 5:
-            print("Enter valid no.")
-display_h2()
-
+            print('Enter valid command.')
